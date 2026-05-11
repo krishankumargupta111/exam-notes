acd2a0e0c21eaf5e98e00251da89bf0a8dc0c6c8
krishankumargupta111 <guptakrishan869@gmail.com>
2026-05-11 21:13:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.post(
 );
 app.use(
   cors({
-    origin:"https://exam-notesfrontend-pko3.onrender.com",
+    origin:"http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
