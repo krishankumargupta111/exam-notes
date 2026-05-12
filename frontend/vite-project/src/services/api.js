66c8d121966c02289d32989864e39881bfdfb3ec
krishankumargupta111 <guptakrishan869@gmail.com>
2026-05-12 17:46:56 +0530

    Update api.js

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -8,6 +8,7 @@ const api=axios.create({
    baseURL:serverUrl})
 api.interceptors.request.use((config)=>{
    const token=localStorage.getItem("token")
+   console.log(token)
    if(token){
       config.headers.Authorization=`Bearer${token}`
    }
