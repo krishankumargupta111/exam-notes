3654ddcc065f4055cc972bc0c7338a917800b3c1
krishankumargupta111 <guptakrishan869@gmail.com>
2026-05-12 14:27:28 +0530

    Update App.jsx

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -9,7 +9,7 @@ import { getCurrentUser } from './services/api'
 import { useDispatch, useSelector } from 'react-redux'
 import PaymentSuccess from './pages/PaymentSuccess'
 import PaymentFailed from './pages/PaymentFailed'
-export const serverUrl="https://exam-notesbackend.onrender.com"
+
 
 function App() {
   const dispatch=useDispatch()
