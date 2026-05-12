df9bf1e831028a774465f350af78cca1aa21fa74
krishankumargupta111 <guptakrishan869@gmail.com>
2026-05-12 11:38:57 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,14 +12,9 @@ export const googleAuth = async (req, res) => {
       });
     }
      let token = await getToken(user._id);
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      path:"/",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-    return res.status(200).json({ user });
+  return res.status(200).json({
+    token,
+    user})
    
   } catch (error) {
     return res.status(500).json({ message: `googleSignup:error ${error}` });
