35c800494dd50e8b23dcbb90da8b20cbc45d72db
krishankumargupta111 <guptakrishan869@gmail.com>
2026-05-13 09:44:51 +0530

    Update connectDb.js

--- backend/utils/connectDb.js
+++ backend/utils/connectDb.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 const connectDb=async()=>{
     try{
+        console.log("MONGODB_URL =", process.env.MONGODB_URL);
+
+    console.log("JWT_SECRET =", process.env.JWT_SECRET);
         await mongoose.connect(process.env.MONGODB_URL)
         console.log("connect to database")
 
